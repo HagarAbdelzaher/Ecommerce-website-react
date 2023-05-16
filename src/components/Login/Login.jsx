cccc383e067c21f24c1db6576d425be173792be2
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2023-05-16 05:38:57 +0300

    login page

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
+import { Input } from "@material-tailwind/react";
+import axios from "axios";
+import { login } from "../../features/slices/authSlice";
+import { useDispatch } from "react-redux";
+
+const Login = () => {
+  const intitalState = {
+    username: "",
+    password: "",
+  };
+  const [values, setValues] = useState(intitalState);
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/account/login/",
+        values
+      );
+      const token = response.data.token;
+      dispatch(login({ ...values, authUser: true, token }));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const isDisabled = !values.username || !values.password;
+
+  return (
+    <div className="grid grid-cols-1 items-center justify-items-center h-screen">
+      <Card className="w-96">
+        <CardHeader
+          variant="gradient"
+          color="blue"
+          className="mb-4 grid h-28 place-items-center"
+        >
+          <Typography variant="h3" color="white">
+            Sign In
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <Input
+            label="username"
+            size="lg"
+            type="text"
+            name="username"
+            value={values.username}
+            onChange={onChange}
+          />
+          <Input
+            label="Password"
+            size="lg"
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={onChange}
+          />
+          <div className="-ml-2.5"></div>
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button
+            variant="gradient"
+            fullWidth
+            disabled={isDisabled}
+            onClick={handleSubmit}
+          >
+            Sign In
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/features/slices/authSlice.jsx
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: JSON.parse(sessionStorage.getItem("authUser")) || {
+      username: "",
+      password: "",
+      authUser: false,
+    },
+  },
+  reducers: {
+    login(state, action) {
+      const userId = action.payload;
+      const userValidation = /^[A-Za-z0-9]{4,10}$/i.test(userId.username);
+      const passwordValidation = /^[A-Za-z0-9]{4,}$/i.test(userId.password);
+      state.user = userId;
+      if (!userValidation || !passwordValidation) {
+        state.user.authUser = false;
+      } else {
+        state.user.authUser = true;
+        const token = userId.token;
+        const saveState = JSON.stringify(userId);
+        sessionStorage.setItem("authUser", saveState);
+      }
+    },
+    logout(state) {
+      state.user = {
+        username: "",
+        password: "",
+        authUser: false,
+      };
+      sessionStorage.clear();
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
