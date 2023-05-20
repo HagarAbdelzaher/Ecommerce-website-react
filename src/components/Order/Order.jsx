7066f1272436e0d63cfd61dd6f01b4623b3e4f00
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2023-05-20 23:13:01 +0300

    success payment and cancel order

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,85 +1,162 @@
 import React, { useEffect } from "react";
-import queryString from "query-string";
-// import { API_URL } from "../../config";
 import { useSelector, useDispatch } from "react-redux";
-import { setOrder } from "../../features/slices/orderSlice";
+import { setOrder, cancelOrderState } from "../../features/slices/orderSlice";
 import { Button } from "@material-tailwind/react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import interceptorInstance from "../../axios";
+import Navbar from "../Navbar/Navbar";
 
 function Order() {
-  // i want to get order from redux
-  const order = useSelector((state) => state.orders.order);
-  const shipping_address = useSelector((state) => state.orders.shipping_address);
-  const order_items = useSelector((state) => state.orders.order_items);
-  const total_price = useSelector((state) => state.orders.total_price);
-  const payment_status = useSelector((state) => state.orders.payment_status);
-  console.log(order);
-  console.log(order.shipping_address);
+  const orders = useSelector((state) => state.orders.order);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   useEffect(() => {
     interceptorInstance
-      .post("orders/create/")
+      .post("orders/payment/success/")
       .then((response) => {
-        console.log(response.data);
         dispatch(setOrder(response.data));
       })
       .catch((error) => console.log(error));
   }, []);
 
-  const payWithStripe = () => {
+  const cancelOrder = (id) => {
     interceptorInstance
-      .post("orders/checkout/")
+      .post(`orders/${id}/cancel`)
       .then((response) => {
         console.log(response.data);
-        dispatch(setOrder(response.data));
+        dispatch(cancelOrderState(id));
       })
       .catch((error) => console.log(error));
   };
 
   return (
     <>
-      <div className="grid grid-cols-1 items-center justify-items-center h-screen bg-white">
-        <div className="w-2/5 mt-n20 p-5 shadow">
-          <div className="m-1 mb-4 grid h-28 place-items-center shadow">
-            <h3 className="mt-4">Order Summary</h3>
+      <Navbar />
+      <div className="container mx-auto mt-10 p-4">
+        <div className="md:shadow-md justify-center px-2">
+          <div className="flex justify-between border-b pb-8 px-5">
+            <h1 className="font-semibold text-2xl">My Orders</h1>
+            <h2 className="font-semibold text-2xl uppercase">
+              {orders.length}
+              {orders.length > 1 ? " Orders" : " Order"}
+            </h2>
           </div>
-          <div className="flex flex-col gap-4">
-            <p>
-              <strong>Total Price: </strong> {total_price}
-            </p>
-            <strong>Shipping Address: </strong>
-            <p>detailed_address: {shipping_address.detailed_address}</p>
-            <p>country: {shipping_address.country}</p>
-            <p>city: {shipping_address.city}</p>
-            <p>street: {shipping_address.street}</p>
-            <p>district: {shipping_address.district}</p>
-            <p>building_no: {shipping_address.building_no}</p>
-            <p>apartment_no: {shipping_address.apartment_no}</p>
-            <p>floor_no: {shipping_address.floor_no}</p>
+          {orders.length ? (
+            <>
+              {orders.map((order, index) => (
+                <div key={order.id} className="md:shadow-md justify-center px-4 py-5 my-5">
+                  <div className="text-blue-500 text-lg pt-4 pb-6 text-center m-auto">
+                    <strong>
+                      <h1>Order {index + 1}</h1>
+                    </strong>
 
-            <strong>Order Items: </strong>
-            {order_items.map((item) => (
-              <div key={item.id}>
-                <p>product: {item.product.name}</p>
-                <p>quantity: {item.quantity}</p>
-                <p>price: {item.price}</p>
-              </div>
-            ))}
-            <p className="py-4">
-              <strong>Payment Status: </strong> {payment_status}
-            </p>
-          </div>
-          {/* Pay button linked with stripe and change the paid attribute */}
-          <Button
-            className="bg-gray-700 font-semibold hover:bg-green-700 py-3 text-sm text-white uppercase w-full"
-            onClick={() => payWithStripe()}
-          >
-            Pay
-          </Button>
+                    <Button
+                      color="red"
+                      className="mt-4"
+                      onClick={() => cancelOrder(order.id)}
+                    >
+                      Cancel Order
+                    </Button>
+                  </div>
+                  <div className=" xs:hidden lg:flex">
+                    <table className="w-full">
+                      <thead className="text-lg py-5 mb-4">
+                        <tr>
+                          <th className="pt-4 text-center col-span-5">
+                            Product
+                          </th>
+                          <th className="pt-4 text-center col-span-3">
+                            Quantity
+                          </th>
+                          <th className="pt-4 text-center col-span-2">Price</th>
+                          <th className="pt-4 text-center col-span-2">
+                            Total Price
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="py-6">
+                        {order.order_items.map((item) => (
+                          <tr key={item.id} className="my-10 text-center py-5">
+                            <td className="col-span-4 py-3 my-2">
+                              <div className="flex flex-col justify-around text-center">
+                                <span className="text-xl">
+                                  {item.product.name}
+                                </span>
+                                <span className="text-red-500 text-sm">
+                                  Zara
+                                </span>
+                              </div>
+                            </td>
+                            <td className="col-span-3 py-3 my-2">
+                              <span className="text-center w-1/5 text-lg">
+                                {item.quantity}
+                              </span>
+                            </td>
+                            <td className="col-span-2 py-3 my-2">
+                              <span className="text-center w-1/5 text-lg">
+                                ${item.product.price}
+                              </span>
+                            </td>
+                            <td className="col-span-2 py-3 my-2">
+                              <span className="text-center w-1/5 text-lg">
+                                ${item.product.price * item.quantity}
+                              </span>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                      <tfoot className="text-lg py-5 mb-5">
+                        <tr>
+                          <th className="pt-4 text-center col-span-5">
+                            <strong>Date</strong>
+                          </th>
+                          <th className="pt-4 text-center col-span-3">
+                            <strong>Status</strong>
+                          </th>
+                          <th className="pt-4 text-center col-span-2">
+                            <strong>Payment</strong>
+                          </th>
+                          <th className="pt-4 text-center col-span-2">
+                            <strong>Total Price</strong>
+                          </th>
+                        </tr>
+                      </tfoot>
+                      <tfoot className="text-lg py-5 mb-4">
+                        <tr>
+                          <th className="pt-4 text-center col-span-5">
+                            <strong>{order.created_at.split("T")[0]}</strong>
+                          </th>
+                          <th className="pt-4 text-center col-span-3">
+                            <strong>{order.status}</strong>
+                          </th>
+                          <th className="pt-4 text-center col-span-2">
+                            <strong>{order.paid ? "Paid" : "Not Paid"}</strong>
+                          </th>
+                          <th className="pt-4 text-center col-span-2">
+                            <strong>${order.total_price}</strong>
+                          </th>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <div className=" bg-white items-center p-10 ">
+              <p className="text-center text-xl">No Orders Yet..</p>
+            </div>
+          )}
         </div>
+      </div>
+      <div>
+        <Link
+          to="/"
+          className="flex font-semibold text-gray-600 text-lg my-10 pl-10"
+        >
+          <i className="fa-solid fa-arrow-left text-gray-600 text-l pr-1 text-lg"></i>
+          Continue Shopping
+        </Link>
       </div>
     </>
   );
